80adb8a0cd26c64fbec95304f5ee008a01629ec7
pc <nikita.shevnin2016@yandex.ru>
2024-07-06 22:05:44 +0300

    step1.3

--- src/main/java/ru/gb/vkr/vkrProject/repository/UserRepository.java
+++ src/main/java/ru/gb/vkr/vkrProject/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package ru.gb.vkr.vkrProject.repository;
 
-import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import ru.gb.vkr.vkrProject.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-public interface Repository extends JpaRepository<Entity, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
+
+
+
