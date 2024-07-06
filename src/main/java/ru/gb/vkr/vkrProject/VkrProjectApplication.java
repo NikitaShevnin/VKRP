15d7733f6b5a036ab1d825b213b9a78b39bfa93e
pc <nikita.shevnin2016@yandex.ru>
2024-07-06 18:47:34 +0300

    start

---
+++ src/main/java/ru/gb/vkr/vkrProject/VkrProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.vkr.vkrProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkrProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkrProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gb/vkr/vkrProject/VkrProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.vkr.vkrProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkrProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
