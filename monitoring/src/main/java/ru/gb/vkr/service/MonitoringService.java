5d36cfaeb6ac3f03eb4ef958dda9aa55b3db4352
pc <nikita.shevnin2016@yandex.ru>
2024-07-14 17:58:29 +0300

    step 2.4 add monitoring

--- monitoring/src/main/java/ru/gb/vkr/service/MonitoringService.java
+++ monitoring/src/main/java/ru/gb/vkr/service/MonitoringService.java
@@ -1,48 +1,63 @@
 package ru.gb.vkr.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.health.HealthEndpoint;
-import org.springframework.boot.actuate.info.InfoEndpoint;
-import org.springframework.boot.actuate.metrics.MetricsEndpoint;
-import org.springframework.stereotype.Service;
-
+import java.lang.management.*;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.stereotype.Service;
+import ru.gb.vkr.model.CustomMetricsEndpoint;
+import ru.gb.vkr.model.MetricResponse;
 
 @Service
 public class MonitoringService {
-
-    private final HealthEndpoint healthEndpoint;
-    private final InfoEndpoint infoEndpoint;
-    private final MetricsEndpoint metricsEndpoint;
+    private final CustomMetricsEndpoint customMetricsEndpoint;
+    private final ConfigurableEnvironment environment;
 
     @Autowired
-    public MonitoringService(HealthEndpoint healthEndpoint, InfoEndpoint infoEndpoint, MetricsEndpoint metricsEndpoint) {
-        this.healthEndpoint = healthEndpoint;
-        this.infoEndpoint = infoEndpoint;
-        this.metricsEndpoint = metricsEndpoint;
-    }
-
-    public String getHealthStatus() {
-        return healthEndpoint.health().getStatus().getCode();
-    }
-
-    public String getMemoryUsage() {
-        return infoEndpoint.info().get("memory").toString();
-    }
-
-    public String getConfigurationProperties() {
-        return infoEndpoint.info().toString();
+    public MonitoringService(CustomMetricsEndpoint customMetricsEndpoint, ConfigurableEnvironment environment) {
+        this.customMetricsEndpoint = customMetricsEndpoint;
+        this.environment = environment;
     }
 
     public List<MetricResponse> getMetrics() {
-        Collection<String> metricNames = metricsEndpoint.getNames();
+        Collection<String> metricNames = customMetricsEndpoint.getNames();
         List<MetricResponse> metrics = new ArrayList<>();
         for (String metricName : metricNames) {
-            MetricResponse metric = metricsEndpoint.find(metricName);
+            MetricResponse metric = customMetricsEndpoint.find(metricName);
             metrics.add(metric);
         }
         return metrics;
     }
 
+    public HealthStatus getHealthStatus() {
+        OperatingSystemMXBean osBean = ManagementFactory.getOperatingSystemMXBean();
+        RuntimeMXBean runtimeBean = ManagementFactory.getRuntimeMXBean();
+
+        return new HealthStatus(
+                osBean.getArch(),
+                osBean.getVersion(),
+                osBean.getAvailableProcessors(),
+                runtimeBean.getUptime()
+        );
+    }
+
+    public MemoryUsage getMemoryUsage() {
+        MemoryMXBean memoryBean = ManagementFactory.getMemoryMXBean();
+
+        return new MemoryUsage(
+                memoryBean.getHeapMemoryUsage().getUsed(),
+                memoryBean.getHeapMemoryUsage().getMax(),
+                memoryBean.getNonHeapMemoryUsage().getUsed(),
+                memoryBean.getNonHeapMemoryUsage().getMax()
+        );
+    }
+
+    public Map<String, Object> getConfigurationProperties() {
+        return environment.getSystemEnvironment();
+    }
 }
